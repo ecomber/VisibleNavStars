20f04cd1615925837acb3802a8c4b588eadaea4f
ecomber <e@comber.org>
2026-03-10 20:08:00 +0000

    First

--- VisbleStars.py
+++ VisbleStars.py
@@ -10,18 +10,26 @@ def hours_to_hms(hours):
     hr, mnt = divmod(mnt, 60)
     return f"{int(hr)}h {int(mnt)}m {round(sec)}s"
 
+def is_between(t, start, end):
+    if start <= end:
+        return start <= t <= end
+    else:  # crosses 0 deg
+        return t >= start or t <= end
 
 if __name__ == '__main__':
-
-    localLat = (38 + 12.34 / 60) # Horta or near enough
-    localLong = -(28 + 12.34 / 60)
-    lowerAlt = 20
-    upperAlt = 50
+    HoFromSextant = 39.5
+    print(f'Sextant height: {AstroCalcs.deg_to_dm(HoFromSextant)}')
+    localLat = 51 # Horta or near enough
+    localLong = -0.5
+    lowerAlt = 5
+    upperAlt = 45
+    leftWindow = 340
+    rightWindow = 45
     minimumMagnitude = 3  # = 3 takes in all
     now = AstroCalcs.now()
     observation_time = datetime.datetime(year=2026, month=5, day=1, hour=22, minute=0, second=0, tzinfo=datetime.timezone.utc)
 
-    targetTime = now  # or observation_time  # + datetime.timedelta(hours=12)
+    targetTime = now # or observation_time  # + datetime.timedelta(hours=12)
     print(f"Navigation Stars between Alt {lowerAlt}° and {upperAlt}°, visible at Lat:{localLat:.2f} Lon:{localLong:.2f}, at {str(targetTime)[:19]} UTC")
     jd = AstroCalcs.julian_day(targetTime)
     gst = AstroCalcs.greenwich_sidereal_time(jd)
@@ -43,17 +51,26 @@ if __name__ == '__main__':
         lha = (lst + sha) % 360
         decl = allNavStars[starName][1]
         mag = allNavStars[starName][2]
-        Alt, Z, Zn = AstroCalcs.sightReduction(localLat, decl, lha)
+        Alt, Zn = AstroCalcs.sightReduction(localLat, decl, lha)
         if lowerAlt < Alt < upperAlt and mag < minimumMagnitude:
+            #if Zn > leftWindow or Zn < rightWindow:
             outputStars.append((starName, Zn, Alt, mag))
     byMagnitude = sorted(outputStars, key=operator.itemgetter(3))
     byAzimuth = sorted(outputStars, key=operator.itemgetter(1))
 
     print("\nBy Magnitude:")
     for item in byMagnitude:
-        print(f'{item[0]:<10}\t\t\tAz:{item[1]:03.0f}°\tAl:{item[2]:02.0f}°\t{AstroCalcs.deg_to_dm(item[2])}\tMag:{item[3]}')
+        asterix  = "*" if abs(item[2] - HoFromSextant) < 0.5 else ""
+        print(f'{item[0]:<10}\t\t\tAz:{item[1]:03.0f}°\tAl:{item[2]:02.0f}°\t{AstroCalcs.deg_to_dm(item[2])} {AstroCalcs.deg_to_dm(item[2] - HoFromSextant)}\tMag:{item[3]}  {asterix}')
 
     print()
     print("\nBy Azimuth:")
     for item in byAzimuth:
-        print(f'{item[0]:<10}\t\t\tAz:{item[1]:03.0f}°\tAl:{item[2]:02.0f}°\t{AstroCalcs.deg_to_dm(item[2])}\tMag:{item[3]}')
+        asterix  = "*" if abs(item[2] - HoFromSextant) < 0.5 else ""
+        print(f'{item[0]:<10}\t\t\tAz:{item[1]:03.0f}°\tAl:{item[2]:02.0f}°\t{AstroCalcs.deg_to_dm(item[2])} {AstroCalcs.deg_to_dm(item[2] - HoFromSextant)}\tMag:{item[3]}  {asterix}')
+
+    print()
+    print("Likely candidates:")
+    for item in byAzimuth:
+        if abs(item[2] - HoFromSextant) < 0.5:
+            print(f'{item[0]:<10}\t\t\tAz:{item[1]:03.0f}°\tAl:{item[2]:02.0f}°\t{AstroCalcs.deg_to_dm(item[2])} {AstroCalcs.deg_to_dm(item[2] - HoFromSextant)}\tMag:{item[3]}  {asterix}')
