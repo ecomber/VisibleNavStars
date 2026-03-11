5d205f5fa4c2c2dd6bc9b609b69b6aa355693579
ecomber <e@comber.org>
2026-03-11 10:17:07 +0000

    Star filter

--- VisbleStars.py
+++ VisbleStars.py
@@ -10,26 +10,37 @@ def hours_to_hms(hours):
     hr, mnt = divmod(mnt, 60)
     return f"{int(hr)}h {int(mnt)}m {round(sec)}s"
 
+
 def is_between(t, start, end):
     if start <= end:
         return start <= t <= end
     else:  # crosses 0 deg
         return t >= start or t <= end
 
+
 if __name__ == '__main__':
-    HoFromSextant = 39.5
+    HoFromSextant = 30
+    MaxIntercept = 1 # degrees !!
     print(f'Sextant height: {AstroCalcs.deg_to_dm(HoFromSextant)}')
-    localLat = 51 # Horta or near enough
+    localLat = 51  # Horta or near enough
     localLong = -0.5
-    lowerAlt = 5
-    upperAlt = 45
+    lowerAlt = 20
+    upperAlt = 50
     leftWindow = 340
     rightWindow = 45
     minimumMagnitude = 3  # = 3 takes in all
-    now = AstroCalcs.now()
-    observation_time = datetime.datetime(year=2026, month=5, day=1, hour=22, minute=0, second=0, tzinfo=datetime.timezone.utc)
 
-    targetTime = now # or observation_time  # + datetime.timedelta(hours=12)
+
+    def displayStar(item, HoFromSextant,HoHcRange):
+        asterix = "*" if abs(item[2] - HoFromSextant) < abs(HoHcRange) else ""
+        # print(f'{item[0]:<15}Az:{item[1]:03.0f}°  Al:{item[2]:02.0f}°  Mag:{item[3]:+5.2f}  {AstroCalcs.deg_to_dm(item[2])} {AstroCalcs.deg_to_dm(item[2] - HoFromSextant)}  {asterix}')
+        print(f'{item[0]:<15}Az:{item[1]:03.0f}°  Mag:{item[3]:+5.2f}  {AstroCalcs.deg_to_dm(item[2])} {AstroCalcs.deg_to_dm(item[2] - HoFromSextant)}  {asterix}')
+
+
+    now = AstroCalcs.now()
+    observation_time = datetime.datetime(year=2026, month=5, day=1, hour=22, minute=0, second=4, tzinfo=datetime.timezone.utc)
+
+    targetTime = now #observation_time  # + datetime.timedelta(hours=12)
     print(f"Navigation Stars between Alt {lowerAlt}° and {upperAlt}°, visible at Lat:{localLat:.2f} Lon:{localLong:.2f}, at {str(targetTime)[:19]} UTC")
     jd = AstroCalcs.julian_day(targetTime)
     gst = AstroCalcs.greenwich_sidereal_time(jd)
@@ -42,7 +53,7 @@ if __name__ == '__main__':
         lines = f.read().splitlines()
     for line in lines:
         l = line.split()
-        allNavStars[l[0]] = (float(l[1]), float(l[2]), NavStarMagnitudes.magnitudes[l[0]])
+        allNavStars[l[0]] = (float(l[1]), float(l[2]), float(NavStarMagnitudes.magnitudes[l[0]]))
 
     outputStars = []
 
@@ -53,24 +64,33 @@ if __name__ == '__main__':
         mag = allNavStars[starName][2]
         Alt, Zn = AstroCalcs.sightReduction(localLat, decl, lha)
         if lowerAlt < Alt < upperAlt and mag < minimumMagnitude:
-            #if Zn > leftWindow or Zn < rightWindow:
+            # if Zn > leftWindow or Zn < rightWindow:
             outputStars.append((starName, Zn, Alt, mag))
     byMagnitude = sorted(outputStars, key=operator.itemgetter(3))
     byAzimuth = sorted(outputStars, key=operator.itemgetter(1))
 
     print("\nBy Magnitude:")
     for item in byMagnitude:
-        asterix  = "*" if abs(item[2] - HoFromSextant) < 0.5 else ""
-        print(f'{item[0]:<10}\t\t\tAz:{item[1]:03.0f}°\tAl:{item[2]:02.0f}°\t{AstroCalcs.deg_to_dm(item[2])} {AstroCalcs.deg_to_dm(item[2] - HoFromSextant)}\tMag:{item[3]}  {asterix}')
+        displayStar(item, HoFromSextant, MaxIntercept)
 
     print()
     print("\nBy Azimuth:")
     for item in byAzimuth:
-        asterix  = "*" if abs(item[2] - HoFromSextant) < 0.5 else ""
-        print(f'{item[0]:<10}\t\t\tAz:{item[1]:03.0f}°\tAl:{item[2]:02.0f}°\t{AstroCalcs.deg_to_dm(item[2])} {AstroCalcs.deg_to_dm(item[2] - HoFromSextant)}\tMag:{item[3]}  {asterix}')
+        displayStar(item, HoFromSextant, MaxIntercept)
 
     print()
     print("Likely candidates:")
     for item in byAzimuth:
-        if abs(item[2] - HoFromSextant) < 0.5:
-            print(f'{item[0]:<10}\t\t\tAz:{item[1]:03.0f}°\tAl:{item[2]:02.0f}°\t{AstroCalcs.deg_to_dm(item[2])} {AstroCalcs.deg_to_dm(item[2] - HoFromSextant)}\tMag:{item[3]}  {asterix}')
+        if abs(item[2] - HoFromSextant) < abs(MaxIntercept):
+            displayStar(item, HoFromSextant, MaxIntercept)
+            if HoFromSextant > item[2]:
+                direction = "Towards"
+            else:
+                direction = "Away"
+
+            # outputTable.append([f"Intercept ({direction})", f"{deg_to_dm(item[2] - HoFromSextant)}"])
+            print(f'  {"Azimuth":<20}{round(item[1]):5.0f} °')
+            dir = f'Intercept({direction})'
+            print(f'  {dir:<18} {abs((item[2] - HoFromSextant) * 60):6.2f} nm')
+            print(f'  Intercept{abs((item[2] - HoFromSextant) * 60):16.2f} nm {direction}')
+            print(f'  LoP{round((item[1]+90)%360):22.0f} °\n')
